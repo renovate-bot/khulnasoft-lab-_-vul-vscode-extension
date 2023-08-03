7b1cf18945b9d76a8d7e1b2ad5f05e569a3462c4
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-08-04 02:33:27 +0600

    Create utils.ts

---
+++ src/explorer/utils.ts
@@ -0,0 +1,41 @@
+
+import { Secret, VulResult } from './vul_result';
+
+
+function getSeverityPosition(severity: string): number {
+    switch (severity) {
+        case 'CRITICAL':
+            return 0;
+        case 'HIGH':
+            return 1;
+        case 'MEDIUM':
+            return 2;
+        case 'LOW':
+            return 3;
+        default:
+            return -1;
+    }
+}
+
+
+
+const sortBySeverity = (a: VulResult, b: VulResult): number => {
+    if (a.extraData instanceof Secret) {
+        return 1;
+    }
+    if (getSeverityPosition(a.severity) > getSeverityPosition(b.severity)) {
+        return 1;
+    } else if (getSeverityPosition(a.severity) < getSeverityPosition(b.severity)) {
+        return -1;
+    }
+
+    return 0;
+};
+
+
+
+
+
+
+
+export { sortBySeverity };
